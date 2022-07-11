7fef30979461a932ad05c7cee5097e115409c0dc
qewer33 <yerdemergul@gmail.com>
2022-07-12 00:50:41 +0300

    Update package

--- package.sh
+++ package.sh
@@ -21,7 +21,8 @@ get_project_name() {
 
 PACKAGE_NAME=$(kreadconfig5 --file="$PWD/package/metadata.desktop" --group="Desktop Entry" --key="X-KDE-PluginInfo-Name")
 
-tar -cvf "${PACKAGE_NAME}.kwinscript" -C "package/" .
+# tar -cvf "${PACKAGE_NAME}.kwinscript" -C "package/" .
+zip -r ${PACKAGE_NAME}.kwinscript package
 
 prompt "Do you want to commit and push the changes to the git repository?" && \
 git add . && \
